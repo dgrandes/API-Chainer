16cd61e0c2015c9d087bda7ca02a4fc5af9e5926
David Grandes <davidg@olx.com>
2012-08-16 18:07:51 -0300

    ajax form

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,19 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 
 object Application extends Controller {
   
+  val queryForm = Form(
+  	single(
+  		"query" -> text
+  		)
+  )
+
   def index = Action {
-    Ok(views.html.index())
+    Ok(views.html.index(queryForm))
   }
   
 }
--- app/controllers/Data.scala
+++ app/controllers/Data.scala
@@ -2,13 +2,18 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 
 object Data extends Controller {
   
-  def search(query: String) = Action {
-    	val data = new Data()
-    	Ok(data.search(query))
-  }
+	def search() = Action { implicit request => {
+	  	val query = Application.queryForm.bindFromRequest.get
+	  	val data = new Data()
+	    Ok(data.search(query))
+	  }
+    }
+  
   
 }
 
@@ -17,7 +22,9 @@ class Data {
 	val data_path = "app/data/data_types/";
 
 	def search(query: String) : String = {
+
 		val filename = data_path+query+".json"
+
 		try{
 			val fileContent = scala.io.Source.fromFile(filename).mkString	
 			return fileContent
